521209d4f3a92ee9ed9d96ad5bc6add5b2772e52
ignacioz21 <168688549+ignacioz21@users.noreply.github.com>
2025-06-10 09:53:05 -0600

    Build PersonalWeb springboot
    Se inicializo el spring boot del proyecto

---
+++ InitializrSpringbootProject/src/main/java/com/webpage/personalWebAndres/PersonalWebAndresApplication.java
@@ -0,0 +1,13 @@
+package com.webpage.personalWebAndres;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalWebAndresApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonalWebAndresApplication.class, args);
+	}
+
+}
